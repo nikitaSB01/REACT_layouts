b8b8f40456521ba499324034e64f79bfb916821f
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-12-27 19:43:56 +0500

    add colors

--- src/components/ShopCard.jsx
+++ src/components/ShopCard.jsx
@@ -1,23 +1,29 @@
 import PropTypes from "prop-types";
 
+// Компонент для отображения одной карточки товара
 function ShopCard({ card }) {
+  // Динамическое формирование класса для цвета текста
+  const colorClass = `color-${card.color.toLowerCase()}`;
+
   return (
     <div className="shop-card">
       <img src={card.img} alt={card.name} />
       <div>{card.name}</div>
-      <div>{card.color}</div>
-      <div>{card.price}</div>
+      {/* Применяем динамический класс для цвета */}
+      <div className={colorClass}>{card.color}</div>
+      <div>${card.price}</div>
       <button>Add to Cart</button>
     </div>
   );
 }
 
+// Пропсы, которые ожидает компонент
 ShopCard.propTypes = {
   card: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    price: PropTypes.string.isRequired,
-    color: PropTypes.string.isRequired,
-    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired, // Название товара
+    price: PropTypes.string.isRequired, // Цена товара
+    color: PropTypes.string.isRequired, // Цвет товара
+    img: PropTypes.string.isRequired, // Ссылка на изображение товара
   }).isRequired,
 };
 
--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -1,23 +1,29 @@
 import PropTypes from "prop-types";
 
+// Компонент для отображения одного товара в списке
 function ShopItem({ item }) {
+  // Динамическое формирование класса для цвета текста
+  const colorClass = `color-${item.color.toLowerCase()}`;
+
   return (
     <div className="shop-item">
       <img src={item.img} alt={item.name} />
       <div>{item.name}</div>
-      <div>{item.color}</div>
+      {/* Применяем динамический класс для цвета */}
+      <div className={colorClass}>{item.color}</div>
       <div>${item.price}</div>
       <button>Add to Cart</button>
     </div>
   );
 }
 
+// Пропсы, которые ожидает компонент
 ShopItem.propTypes = {
   item: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    price: PropTypes.string.isRequired,
-    color: PropTypes.string.isRequired,
-    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired, // Название товара
+    price: PropTypes.string.isRequired, // Цена товара
+    color: PropTypes.string.isRequired, // Цвет товара
+    img: PropTypes.string.isRequired, // Ссылка на изображение товара
   }).isRequired,
 };
 
